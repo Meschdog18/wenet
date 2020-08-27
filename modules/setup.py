24b05cff1708b7e6a4f647114bf0ac3d60e6ee08
Jordan Mesches <zippedzipper5@gmail.com>
2020-08-27 19:22:12 -0400

    init with setup

---
+++ modules/setup.py
@@ -0,0 +1,18 @@
+#setup flow
+def choice():
+    print("Would you like to start a new network or join a network?")
+    print("Type N, to start a new network")
+    print("Type J, to join a network")
+    ine = input()
+    if ine.lower() == "n":
+        pass
+    if ine.lower() == "j":
+        print("ok")
+    else:
+        print("INVALID INPUT")
+        choice()
+def new():
+    print("new network")
+def join():
+    print("join network")
+    
