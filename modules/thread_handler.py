f4da283aeccc73ae63ea8d9b7e303a2796f28347
shitcode6 <70351049+shitcode6@users.noreply.github.com>
2020-08-28 11:36:47 -0400

    Update thread_handler.py

--- modules/thread_handler.py
+++ modules/thread_handler.py
@@ -2,20 +2,20 @@ import threading
 from modules.client import Client
 from modules.server import Server
 
-def spool_up():
-    connectTo = input("Enter address (address:port): ")
-    if connectTo == "":
-        connectTo = "2.tcp.ngrok.io:14583"
-    host = connectTo.split(":")[0]
-    port = connectTo.split(":")[1]
-    myclient = Client(host, int(port))
+def spool_up(doJoin):
+    if doJoin:
+        connectTo = input("Enter address (address:port), or enter 'h' for host only mode: ")
+        if connectTo == "":
+            connectTo = "2.tcp.ngrok.io:14583"
+        host = connectTo.split(":")[0]
+        port = connectTo.split(":")[1]
+        print(host)
+        print(port)
+        myclient = Client(host, int(port))#change to whatever ngrok tunnels to
+        client = threading.Thread(target=myclient.start, args=())
+        client.start()
+        client.join()
     myserver = Server("localhost", 8000)
-
-    client = threading.Thread(target=myclient.start, args=())
     server = threading.Thread(target=myserver.start, args=())
-
-    client.start()
     server.start()
-
-    client.join()
     server.join()
