aa7900f162093bbaf01baf30d412fae9ababee8c
shitcode6 <70351049+shitcode6@users.noreply.github.com>
2020-08-28 11:24:33 -0400

    Allow user to input ip

--- modules/thread_handler.py
+++ modules/thread_handler.py
@@ -3,7 +3,14 @@ from modules.client import Client
 from modules.server import Server
 
 def spool_up():
-    myclient = Client("2.tcp.ngrok.io", 14583)#change to whatever ngrok tunnels to
+    connectTo = input("Enter address (address:port): ")
+    if connectTo == "":
+        connectTo = "2.tcp.ngrok.io:14583"
+    host = connectTo.split(":")[0]
+    port = connectTo.split(":")[1]
+    print(host)
+    print(port)
+    myclient = Client(host, int(port))
     myserver = Server("localhost", 8000)
 
     client = threading.Thread(target=myclient.start, args=())
